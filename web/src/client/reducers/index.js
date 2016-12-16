32949dfb857d507aed793bb99914a15a4b8ef8df
Christopher Dunavan <github@hypersprite.com>
2016-12-15 17:08:15 -0800

    redux - resets local state on logout

--- web/src/client/reducers/index.js
+++ web/src/client/reducers/index.js
@@ -3,15 +3,23 @@
 import { combineReducers } from 'redux';
 import { reducer as form } from 'redux-form';
 
+import { TYPES } from '../actions';
 import auth from './auth';
 import todos from './todos';
 import visibilityFilter from './visibility-filter';
 
-const app = combineReducers({
+const appReducer = combineReducers({
   auth,
   form,
   todos,
   visibilityFilter,
 });
 
-export default app;
+const rootReducer = (state, action) => {
+  if (action.type === TYPES.UNAUTH_USER) {
+    state = undefined;
+  }
+  return appReducer(state, action);
+};
+
+export default rootReducer;
