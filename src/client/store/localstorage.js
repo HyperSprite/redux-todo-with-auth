8c31f75d6c90ba53ffcc461bc3e4085147a8e7db
Christopher Dunavan <github@hypersprite.com>
2016-12-15 17:09:23 -0800

    update to localstorage to use serialize-javascript package

---
+++ src/client/store/localstorage.js
@@ -0,0 +1,22 @@
+import serialize from 'serialize-javascript';
+
+export const loadState = () => {
+  try {
+    const serializedState = localStorage.getItem('state');
+    if (serializedState === null) {
+      return undefined;
+    }
+    return JSON.parse(serializedState);
+  } catch (err) {
+    return undefined;
+  }
+};
+
+export const saveState = (state) => {
+  try {
+    const serializedState = serialize(state, { isJSON: true });
+    localStorage.setItem('state', serializedState);
+  } catch (err) {
+    // do nothing
+  }
+};
