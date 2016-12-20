f3b01d92ed1f99742df28bdbb3085c87d5338a9c
Christopher Dunavan <github@hypersprite.com>
2016-12-19 17:24:09 -0800

    more updates to events

--- src/server/models/events.js
+++ src/server/models/events.js
@@ -6,10 +6,14 @@ const Schema = mongoose.Schema;
 const eventSchema = new Schema({
   eventTitle: String,
   eventDate: String,
+  eventLocCity: String,
+  eventLocState: String,
+  eventLocCountry: String,
+  eventStartElevation: Number,
   eventURL: { type: String, lower: true },
   eventDesc: String,
   eventRouteURL: { type: String, lower: true },
-  eventType: String,
+  eventType: String, // run ride, gravel, trail, etc.
 });
 
 eventSchema.plugin(findOrCreate);
