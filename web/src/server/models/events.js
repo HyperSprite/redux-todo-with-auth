805b4e7c182c83dca4db2c49786ccf6363091413
Christopher Dunavan <github@hypersprite.com>
2016-12-19 17:18:48 -0800

    events model updates

--- web/src/server/models/events.js
+++ web/src/server/models/events.js
@@ -1,12 +1,19 @@
 const mongoose = require('mongoose');
-const ObjectId = mongoose.Schema.Types.ObjectId;
 const findOrCreate = require('mongoose-findorcreate');
 
 const Schema = mongoose.Schema;
 
-const eventSchema = new Schema ({
+const eventSchema = new Schema({
   eventTitle: String,
   eventDate: String,
-  eventURL: String,
-  eventDesc: 
+  eventURL: { type: String, lower: true },
+  eventDesc: String,
+  eventRouteURL: { type: String, lower: true },
+  eventType: String,
 });
+
+eventSchema.plugin(findOrCreate);
+
+const ModelClass = mongoose.model('event', eventSchema);
+
+module.exports = ModelClass;
