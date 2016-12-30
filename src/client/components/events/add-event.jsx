81da40f78fa450bc3bafd0227e8dfdfc24b6c904
Christopher Dunavan <github@hypersprite.com>
2016-12-30 14:40:48 -0800

    cleenup of styles

--- src/client/components/events/add-event.jsx
+++ src/client/components/events/add-event.jsx
@@ -1,6 +1,6 @@
 import React, { Component, PropTypes } from 'react';
 import { Field, reduxForm } from 'redux-form';
-import { RaisedButton, FlatButton } from 'material-ui';
+import { FlatButton, Paper, RaisedButton } from 'material-ui';
 import { DatePicker, TextField } from 'redux-form-material-ui';
 import { connect } from 'react-redux';
 import { Redirect } from 'react-router';
@@ -10,10 +10,7 @@ import validate from './../form/validate';
 import Alert from './../form/alert';
 // import DatePicker from './../form/datepicker';
 import Input from './../form/input';
-
-const style = {
-  margin: 12,
-};
+import style from '../../styles/style';
 
 const propTypes = {
 
@@ -34,7 +31,6 @@ let AddEvent = class AddEvent extends Component {
     }
   }
 
-
   render() {
     const { handleSubmit, authenticated, pristine, reset, submitting } = this.props;
     const newDate = new Date();
@@ -44,106 +40,134 @@ let AddEvent = class AddEvent extends Component {
       );
     }
     return (
-      <form onSubmit={handleSubmit(this.handleFormSubmit.bind(this))}>
-        <div>
-          <Field
-            component={TextField}
-            floatingLabelText="Title"
-            name="eventTitle"
-            type="text"
-            hintText="Event Title"
-          />
-        </div>
-        <div>
-          <Field
-            component={DatePicker}
-            floatingLabelText="Event Date"
-            name="eventDate"
-            format={null}
-            onChange={(value) => {
-              console.log('date changed ', value) // eslint-disable-line no-console
-            }}
-            hintText="Event Day?"
-          />
-        </div>
-        <div>
-          <Field
-            component={TextField}
-            floatingLabelText="City"
-            name="eventLocCity"
-            type="text"
-            hintText="Type the City"
-          />
-        </div>
-        <div>
-          <Field
-            component={TextField}
-            floatingLabelText="State"
-            name="eventLocState"
-            type="text"
-            hintText="Type the State"
-          />
-        </div>
-        <div>
-          <Field
-            component={TextField}
-            floatingLabelText="Country"
-            name="eventLocCountry"
-            type="text"
-            hintText="Type the Country"
-          />
-        </div>
-        <div>
-          <Field
-            component={TextField}
-            floatingLabelText="Starting Elevation"
-            name="eventStartElevation"
-            type="number"
-            hintText="Meters"
-          />
-        </div>
-        <div>
-          <Field
-            component={TextField}
-            floatingLabelText="Event URL"
-            name="eventURL"
-            type="text"
-            hintText="Link to Event Homepage"
-          />
-        </div>
-        <div>
-          <Field
-            component={TextField}
-            floatingLabelText="Description"
-            name="eventDesc"
-            type="text"
-            hintText="Event Description"
-          />
-        </div>
-        <div>
-          <Field
-            component={TextField}
-            floatingLabelText="Route"
-            name="eventRouteURL"
-            type="text"
-            hintText="Type the Strava route link"
-          />
-        </div>
-        <div>
-          <Field
-            component={TextField}
-            floatingLabelText="Type"
-            name="eventType"
-            type="text"
-            floatingLabelText="Type"
-          />
-        </div>
-        { this.renderAlert() }
-        <div>
-          <RaisedButton type="submit" label="Submit" primary={true} style={style} disabled={pristine || submitting} />
-          <FlatButton type="button" label="Clear Values" style={style} disabled={pristine || submitting} onClick={reset} />
-        </div>
-      </form>
+      <Paper
+        style={style.paper1}
+        zDepth={1}
+      >
+        <form onSubmit={handleSubmit(this.handleFormSubmit.bind(this))}>
+          <div>
+            <Field
+              component={TextField}
+              style={style.formelement}
+              floatingLabelText="Title"
+              name="eventTitle"
+              type="text"
+              hintText="Event Title"
+            />
+          </div>
+          <div>
+            <Field
+              component={DatePicker}
+              style={style.formelement}
+              floatingLabelText="Event Date"
+              name="eventDate"
+              format={null}
+              onChange={(value) => {
+                console.log('date changed ', value) // eslint-disable-line no-console
+              }}
+              hintText="Event Day?"
+            />
+          </div>
+          <div>
+            <Field
+              component={TextField}
+              style={style.formelement}
+              floatingLabelText="City"
+              name="eventLocCity"
+              type="text"
+              hintText="Type the City"
+            />
+          </div>
+          <div>
+            <Field
+              component={TextField}
+              style={style.formelement}
+              floatingLabelText="State"
+              name="eventLocState"
+              type="text"
+              hintText="Type the State"
+            />
+          </div>
+          <div>
+            <Field
+              component={TextField}
+              style={style.formelement}
+              floatingLabelText="Country"
+              name="eventLocCountry"
+              type="text"
+              hintText="Type the Country"
+            />
+          </div>
+          <div>
+            <Field
+              component={TextField}
+              style={style.formelement}
+              floatingLabelText="Starting Elevation"
+              name="eventStartElevation"
+              type="number"
+              hintText="Meters"
+            />
+          </div>
+          <div>
+            <Field
+              component={TextField}
+              style={style.formelement}
+              floatingLabelText="Event URL"
+              name="eventURL"
+              type="text"
+              hintText="Link to Event Homepage"
+            />
+          </div>
+          <div>
+            <Field
+              component={TextField}
+              style={style.formelement}
+              floatingLabelText="Description"
+              name="eventDesc"
+              type="text"
+              hintText="Event Description"
+            />
+          </div>
+          <div>
+            <Field
+              component={TextField}
+              style={style.formelement}
+              floatingLabelText="Route"
+              name="eventRouteURL"
+              type="text"
+              hintText="Type the Strava route link"
+            />
+          </div>
+          <div>
+            <Field
+              component={TextField}
+              style={style.formelement}
+              floatingLabelText="Type"
+              name="eventType"
+              type="text"
+              floatingLabelText="Type"
+            />
+          </div>
+          { this.renderAlert() }
+          <div>
+            <RaisedButton
+              type="submit"
+              label="Submit"
+              primary={true}
+              style={style.button}
+              disabled={pristine || submitting}
+            />
+            <FlatButton
+              type="button"
+              label="Clear Values"
+              style={style.button}
+              disabled={pristine || submitting}
+              onClick={reset}
+            />
+          </div>
+        </form>
+      </Paper>
+
     );
   }
 };
