7ff12f5ae3fa5cf34af006d061e29fd3b24a7df5
Christopher Dunavan <github@hypersprite.com>
2017-01-15 13:03:06 -0800

    reqork webpack.config for single file

--- web/src/server/index.js
+++ web/src/server/index.js
@@ -55,7 +55,7 @@ if (!hlpr.isProd()) {
   const webpack = require('webpack');
   const webpackDevMiddleware = require('webpack-dev-middleware');
   const webpackHotMiddleware = require('webpack-hot-middleware');
-  const webpackConfig = require('../../webpack.dev.config');
+  const webpackConfig = require('../../webpack.config');
   const compiler = webpack(webpackConfig);
   app.use(webpackDevMiddleware(compiler, {
     publicPath: webpackConfig.output.publicPath,
--- web/webpack.config.js
+++ web/webpack.config.js
@@ -1,17 +1,114 @@
 const webpack = require('webpack');
 
+const isProd = (process.env.NODE_ENV === 'production');
+
+function getPlugins() {
+  const plugins = [];
+  plugins.push(new webpack.DefinePlugin({
+    'process.env': {
+      NODE_ENV: JSON.stringify(process.env.NODE_ENV),
+    }
+  })
+);
+
+  if (isProd) {
+    plugins.push(new webpack.optimize.UglifyJsPlugin());
+  } else {
+    plugins.push(new webpack.HotModuleReplacementPlugin());
+  }
+  return plugins;
+}
+
+function getEntry() {
+  const entry = [];
+  if (!isProd) {
+    entry.push('webpack-hot-middleware/client');
+  }
+  entry.push('./src/client/index.jsx');
+  entry.push('babel-polyfill');
+
+  return entry;
+}
+
+function getOutput() {
+  const output = {};
+  if (isProd) {
+    output.path = './src/server/public/assets/';
+  } else {
+    output.path = '/../../public/assets/';
+  }
+  output.publicPath = '/assets/';
+  output.filename = 'bundle.js';
+
+  return output;
+}
+
+function getPreLoaders() {
+  const preLoaders = [];
+
+  if (!isProd) {
+    preLoaders.push({
+      test: /\.js$/,
+      loader: 'source-map-loader',
+    });
+  }
+  return preLoaders;
+}
+
+function getLoaders() {
+  const loaders = [];
+  if (isProd) {
+    loaders.push({
+      test: /\.(js|jsx)$/,
+      exclude: /node_modules/,
+      loader: 'babel',
+      query: {
+        plugins: ['transform-flow-strip-types', 'transform-runtime'],
+        presets: ['es2015', 'stage-0', 'react'],
+      },
+    });
+  } else {
+    loaders.push({
+      test: /\.(js|jsx)$/,
+      exclude: /node_modules/,
+      loaders: ['react-hot-loader/webpack', 'babel'],
+    });
+  }
+  // common loaders
+  loaders.push({
+    test: /\.css$/,
+    loader: 'style-loader!css-loader',
+  });
+  loaders.push({
+    test: /\.less/,
+    loader: 'style!css!less?rootpath=/assets',
+  });
+  loaders.push({
+    test: /\.gif$/,
+    loader: 'url-loader?mimetype=image/png',
+  });
+  loaders.push({
+    test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+    loader: 'url-loader?limit=10000&minetype=application/font-woff',
+  });
+  loaders.push({
+    test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+    loader: 'file-loader?name=[name].[ext]',
+  });
+
+  return loaders;
+}
+
+function getDevtool() {
+  const devtool = isProd ? null : 'source-map';
+  return devtool;
+}
+
 module.exports = {
-  entry: [
-    // Set up an ES6-ish environment
-    'babel-polyfill',
-    // Add your application's scripts below
-    './src/client/index.jsx',
-  ],
-  output: {
-    path: './src/server/public/assets/',
-    publicPath: '/assets/',
-    filename: 'bundle.js',
-  },
+  devtool: getDevtool(),
+  context: __dirname,
+  entry: getEntry(),
+  output: getOutput(),
   resolve: {
     extensions: [
       '',
@@ -21,49 +118,9 @@ module.exports = {
       '.js',
     ],
   },
-  plugins: [
-    new webpack.DefinePlugin({
-      'process.env': {
-        NODE_ENV: JSON.stringify('production'),
-      },
-    }),
-    new webpack.optimize.UglifyJsPlugin({
-      compress: {
-        warnings: true,
-      },
-    }),
-  ],
+  plugins: getPlugins(),
   module: {
-    loaders: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        loader: 'babel',
-        query: {
-          plugins: ['transform-flow-strip-types', 'transform-runtime'],
-          presets: ['es2015', 'stage-0', 'react'],
-        },
-      },
-      {
-        test: /\.css$/,
-        loader: 'style-loader!css-loader',
-      },
-      {
-        test: /\.less/,
-        loader: 'style!css!less?rootpath=/assets',
-      },
-      {
-        test: /\.gif$/,
-        loader: 'url-loader?mimetype=image/png',
-      },
-      {
-        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-        loader: 'url-loader?limit=10000&minetype=application/font-woff',
-      },
-      {
-        test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-        loader: 'file-loader?name=[name].[ext]',
-      },
-    ],
+    loaders: getLoaders(),
+    preLoaders: getPreLoaders(),
   },
 };
