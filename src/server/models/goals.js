92af598bf4fda695403a3484f83dd0d20eba8a2e
Christopher Dunavan <github@hypersprite.com>
2017-01-15 11:08:38 -0800

    starting on goals

---
+++ src/server/models/goals.js
@@ -0,0 +1,52 @@
+const mongoose = require('mongoose');
+const findOrCreate = require('mongoose-findorcreate');
+const serialize = require('serialize-javascript');
+const uuidv4 = require('uuid/v4');
+
+const hlpr = require('../lib/helpers');
+
+const Schema = mongoose.Schema;
+const ObjectId = mongoose.Schema.Types.ObjectId;
+
+const goalNotesSchema = new Schema(
+  {
+    goalNoteId: { type: String, default: `id-${uuidv4()}` },
+    goalNoteContent: String,
+    goalNoteCreated: String,
+    goalNoteUpdated: String,
+    goalNotePrivate: { type: Boolean, default: false },
+    goalNoteDeleted: { type: Boolean, default: false },
+  },
+);
+
+
+const goalSchema = new Schema(
+  {
+    goalId: { type: String, default: `id-${uuidv4()}` },
+    goalEventId: String,
+    goalOwner: Number, // stravaId of user that creates goal
+    goalTitle: String,
+    goalMotivation: String, // Why are you doing this?
+    goalStartTrainingDate: String, // default to creation date?
+    goalRank: { type: String, enum: ['', 'A', 'B', 'C'], default: '' }, // a race, b race, c race,
+    goalCalendarLink: String, // URL to personal calendar
+    goalPrivate: { type: Boolean, default: false },
+    goalDeleted: { type: Boolean, default: false },
+    goalNotes: [goalNotesSchema],
+  },
+  {
+    timestamps: true,
+  });
+
+// saving this for later
+// goalSchema.pre('save', function goalSchemaPre(next) {
+//   const goal = this;
+//   goal.goalTitle = goal.goalTitle;
+//   next();
+// });
+
+goalSchema.plugin(findOrCreate);
+
+const goals = mongoose.model('goals', goalSchema);
+
+module.exports = goals;
