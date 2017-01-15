564208d0bc65a338ddb9ccde07d7d1941613ae8a
Christopher Dunavan <github@hypersprite.com>
2017-01-15 11:13:42 -0800

    removed source maps from production webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,6 @@
 const webpack = require('webpack');
 
 module.exports = {
-  devtool: 'source-map',
   entry: [
     // Set up an ES6-ish environment
     'babel-polyfill',
@@ -65,14 +64,6 @@ module.exports = {
       {
         test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
         loader: 'file-loader?name=[name].[ext]',
-      },
-    ],
-    preLoaders: [
-      // All output '.js' files will have any sourcemaps re-processed by
-      // 'source-map-loader'.
-      {
-        test: /\.js$/,
-        loader: 'source-map-loader',
       },
     ],
   },
