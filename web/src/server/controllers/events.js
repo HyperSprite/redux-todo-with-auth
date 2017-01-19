6ec04a924e811b13f5bf630d19bf8fe75a8475bb
Christopher Dunavan <github@hypersprite.com>
2017-01-19 10:19:32 -0800

    Added geocode for new and edited events

--- web/src/server/controllers/events.js
+++ web/src/server/controllers/events.js
@@ -1,29 +1,71 @@
 const moment = require('moment');
+const NodeGeocoder = require('node-geocoder');
 
 const Events = require('../models/events');
 const hlpr = require('../lib/helpers');
 
+// GEOCODER --------------------------------------------------------------------
+const geoOptions = {
+  provider: 'google',
+
+  // Optional depending on the providers
+  httpAdapter: 'https', // Default
+  apiKey: process.env.GEOCODER_API_KEY, // for Mapquest, OpenCage, Google Premier
+  formatter: null,         // 'gpx', 'string', ...
+};
+
+const geocoder = NodeGeocoder(geoOptions);
+
+const callGeoCoder = (toSave, event, res) => {
+  const result = toSave;
+  const geoQuery = `${toSave.eventLocStreet || event.eventLocStreet || ''},
+  ${toSave.eventLocCity || event.eventLocCity || ''},
+  ${toSave.eventLocState || event.eventLocState || ''},
+  ${toSave.eventLocCountry || event.eventLocCountry || ''},
+  ${toSave.eventLocZip || event.eventLocZip || ''},`;
+  hlpr.consLog([geoQuery]);
+
+  geocoder.geocode(geoQuery, (err, geoRes) => {
+    if (geoRes.length === 0 || err) return res(err, toSave);
+    hlpr.consLog(['callGeoCoder', 'geoRes', geoRes, 'err', err]);
+    if (geoRes[0] && geoRes[0].extra.confidence >= 0.9) {
+      result.eventGeoFormattedAddress = geoRes[0].formattedAddress;
+      result.eventLocCity = geoRes[0].city;
+      result.eventLocState = geoRes[0].administrativeLevels.level1long;
+      result.eventLocCountry = geoRes[0].country;
+      result.eventLocZip = geoRes[0].zipcode;
+    }
+    result.eventGeoLatitude = geoRes[0].latitude;
+    result.eventGeoLongitude = geoRes[0].longitude;
+    result.eventGeoLevel1Long = geoRes[0].administrativeLevels.level1long;
+    result.eventGeoLevel2Long = geoRes[0].administrativeLevels.level2long;
+    result.eventGeoStreetNumber = geoRes[0].streetNumber;
+    result.eventGeoStreetName = geoRes[0].streetName;
+    result.eventGeoCity = geoRes[0].city;
+    result.eventGeoCountry = geoRes[0].country;
+    result.eventGeoCountryCode = geoRes[0].countryCode;
+    result.eventGeoZipCode = geoRes[0].zipcode;
+    result.eventGeoProvider = geoRes[0].provider;
+    return res(err, result);
+  });
+};
+
+// GEOCODER --------------------------------------------------------------------
+
 exports.addEvent = (req, res) => {
-  const toSave = req.body;
-
-  toSave.eventOwner = req.user.stravaId;
-
-  hlpr.consLog([
-    'events.addEvent',
-    'toSave',
-    toSave,
-  ]);
-
-  Events.create(toSave, (err, event) => {
-    if (!err) {
-      hlpr.consLog(['Event saved']);
-      const result = { event: { postSuccess: true }, updated: event };
-      hlpr.consLog([result]);
-      res.send(result);
-    } else {
+  callGeoCoder(req.body, {}, (err, toSave) => {
+    toSave.eventOwner = req.user.stravaId;
+    hlpr.consLog(['events.addEvent', 'toSave', toSave]);
+    Events.create(toSave, (err, event) => {
+      if (!err) {
+        hlpr.consLog(['Event saved']);
+        const result = { event: { postSuccess: true }, updated: event };
+        hlpr.consLog([result]);
+        return res.send(result);
+      }
       hlpr.consLog(['Event error', err]);
       return res.status(400).send(err);
-    }
+    });
   });
 };
 
@@ -31,14 +73,16 @@ exports.editEvent = (req, res) => {
   Events.findOne({ eventId: req.params.eventId }, (err, event) => {
     if (!event) return res.status(404).send(`Event: ${req.params.eventId} not found`);
     if (event.eventOwner === req.user.stravaId || req.user.adminMember) {
-      const options = { new: true };
-      const action = req.body;
-      Events.findByIdAndUpdate(event._id, action, options, (err, eventEdit) => {
-        if (err) hlpr.consLog(['editEvent', err]);
-        hlpr.consLog(['editEvent', eventEdit.eventId]);
-        const result = { event: { postSuccess: true }, updated: eventEdit };
-        hlpr.consLog([result]);
-        res.send(result);
+      callGeoCoder(req.body, event, (err, toSave) => {
+        hlpr.consLog(['callGeoCoder', err, toSave]);
+        const options = { new: true };
+        Events.findByIdAndUpdate(event._id, toSave, options, (err, eventEdit) => {
+          if (err) hlpr.consLog(['editEvent', err]);
+          hlpr.consLog(['editEvent', eventEdit.eventId]);
+          const result = { event: { postSuccess: true }, updated: eventEdit };
+          hlpr.consLog([result]);
+          res.send(result);
+        });
       });
     } else {
       res.status(401).send('No Access');
