2ce75bb73a15b3d2f662caecc1fa183749929f5b
Christopher Dunavan <github@hypersprite.com>
2017-01-21 11:37:32 -0800

    saving filter state to local storage

--- web/src/server/router/index.js
+++ web/src/server/router/index.js
@@ -17,7 +17,7 @@ const indexHTML = `
     <head>
       <meta http-equiv="X-UA-Compatible" content="IE=edge">
       <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-      <title>A Race Athlete</title>
+      <title>A Race athlete</title>
       <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png">
       <link rel="icon" type="image/png" href="/favicons/favicon-32x32.png" sizes="32x32">
       <link rel="icon" type="image/png" href="/favicons/favicon-16x16.png" sizes="16x16">
