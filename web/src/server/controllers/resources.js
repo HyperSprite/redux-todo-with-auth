4e6cd19cc107b1790bd79b8a4ee5ebb0193f3e57
Christopher Dunavan <github@hypersprite.com>
2017-03-16 08:01:52 -0700

    added astrophases basic return, still needs styles, added C to footer

--- web/src/server/controllers/resources.js
+++ web/src/server/controllers/resources.js
@@ -5,10 +5,10 @@ const hlpr = require('../lib/helpers');
 // input.loc: '-122.1439698,37.426941'
 // input.date: '2017-02-03T19:15:03Z' // date string
 // test with Postman
+// localhost:3080/apiv1/resource/astrophases/?loc=-122.1439698,37.426941&date=3/20/2017&tzOffset=-28800
 // localhost:3080/apiv1/resource/timezone/?loc=-122.1439698,37.426941&date=1458000000
 // localhost:3080/apiv1/resource/elevation/?loc=-122.1439698,37.426941
 // localhost:3080/apiv1/resource/weatherforcast/?loc=-122.1439698,37.426941
-// localhost:3080/apiv1/resource/astrophases/?loc=-122.1439698,37.426941&date=Thu, 09 Mar 2017 21:18:21 GMT&tzOffset=-28800&dstOffset=0
 
 exports.resource = (req, res) => {
   // const loc = req.params.loc.split(',');
@@ -19,6 +19,7 @@ exports.resource = (req, res) => {
     tzOffset: req.query.tzOffset, // in seconds -28800 (-8 hours)
     dstOffset: req.query.dstOffset, // in seconds 3600 ( 1 hour)
   };
+  hlpr.consLog(['controller, resources input', input]);
 
   resorces.rLonLat(input, input.resTarget, output => res.send(output));
 };
