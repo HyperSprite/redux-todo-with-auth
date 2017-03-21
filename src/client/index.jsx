dd6a6d1496274c38946ac022c6e2f0ebb85d9af8
Christopher Dunavan <github@hypersprite.com>
2017-03-21 10:18:13 -0700

    Moved to webpack2, little more work on astro

--- src/client/index.jsx
+++ src/client/index.jsx
@@ -7,6 +7,8 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import { getMuiTheme, MuiThemeProvider } from 'material-ui/styles';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 
+import { AppContainer } from 'react-hot-loader';
+
 import routes from './router';
 import store from './store';
 import Theme from './styles/theme';
@@ -15,13 +17,26 @@ import App from './components/app';
 
 injectTapEventPlugin();
 
-render(
-  <Provider store={store}>
-    <Router>
-      <MuiThemeProvider muiTheme={getMuiTheme(Theme)}>
-        <App >{ routes }</App>
-      </MuiThemeProvider>
-    </Router>
-  </Provider>,
-  document.getElementById('root'),
-);
+const renderApp = (Component) => {
+  render(
+    <AppContainer>
+      <Provider store={store}>
+        <Router>
+          <MuiThemeProvider muiTheme={getMuiTheme(Theme)}>
+            <Component >{ routes }</Component>
+          </MuiThemeProvider>
+        </Router>
+      </Provider>
+    </AppContainer>,
+    document.getElementById('root'),
+  );
+};
+
+renderApp(App);
+
+// Hot Module Replacement API
+if (module.hot) {
+  module.hot.accept('./components/app', () => {
+    renderApp(App);
+  });
+}
