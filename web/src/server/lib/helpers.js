ae8cd52fe4292116775054d0a04717f6dbd93c93
Christopher Dunavan <github@hypersprite.com>
2017-05-08 16:29:13 -0700

    change cron structure

--- web/src/server/lib/helpers.js
+++ web/src/server/lib/helpers.js
@@ -7,18 +7,6 @@ exports.isProd = () => {
   }
 };
 
-// If LOGGING = true
-// This takes an array for argumetns
-// For every element of the array,
-// a console.log message is generated.
-exports.consLog = (arr) => {
-  if (process.env.LOGGING === 'true') {
-    [].slice.call(arr).forEach((arg) => {
-      console.log('>>> ', arg);
-    });
-  }
-};
-
 exports.getDate = (result) => {
   const newDate = moment().utc().format();
   newDate.toString();
@@ -29,4 +17,16 @@ exports.getDate = (result) => {
 exports.correctedTZDate = (stringDate) => {
   const d = new Date(stringDate);
   return `${d.getUTCHours()}:${d.getUTCMinutes()}`;
+};
+
+// If LOGGING = true
+// This takes an array for argumetns
+// For every element of the array,
+// a console.log message is generated.
+exports.consLog = (arr) => {
+  if (process.env.LOGGING === 'true') {
+    [].slice.call(arr).forEach((arg) => {
+      console.log(exports.getDate(r => r), arg);
+    });
+  }
 };
