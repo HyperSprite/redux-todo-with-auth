9a448b76a544b83e4fbd88285732c2c605fc7597
Christopher Dunavan <github@hypersprite.com>
2017-06-14 10:09:25 -0700

    updaing iframe component to show loader bar

--- web/src/client/components/form/iframe.jsx
+++ web/src/client/components/form/iframe.jsx
@@ -1,4 +1,7 @@
-import React, { Component, PropTypes } from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+import { LinearProgress } from 'material-ui';
 
 const propTypes = {
   src: PropTypes.string.isRequired,
@@ -14,20 +17,24 @@ export default class FullheightIframe extends Component {
 
   render() {
     return (
-      <iframe
-        style={{ maxWidth: '100%', width: '100%', height: this.state.iFrameHeight, overflow: 'visible' }}
-        onLoad={() => {
-          this.setState({
-            iFrameHeight: `${this.iframe.contentWindow.document.body.scrollHeight}px`,
-          });
-        }}
-        ref={(c) => { this.iframe = c; }}
-        src={this.props.src}
-        width="100%"
-        height={this.state.iFrameHeight}
-        scrolling="no"
-        frameBorder="0"
-      />
+      <div>
+        <iframe
+          style={{ maxWidth: '100%', width: '100%', height: this.state.iFrameHeight, overflow: 'visible' }}
+          onLoad={() => {
+            this.setState({
+              iFrameHeight: `${this.iframe.contentWindow.document.body.scrollHeight}px`,
+            });
+          }}
+          ref={(c) => { this.iframe = c; }}
+          src={this.props.src}
+          width="100%"
+          height={this.state.iFrameHeight}
+          scrolling="no"
+          frameBorder="0"
+        />
+        {(this.state.iFrameHeight === '0px') ? (<LinearProgress mode="indeterminate" />) : null }
+      </div>
+
     );
   }
 }
