576df1613a1e7825a16a71ed032c9b6acc8cad19
Christopher Dunavan <github@hypersprite.com>
2017-06-23 19:44:25 -0700

    update power at altitude for non-prem users

---
+++ web/src/client/components/form/checkbox-icon.jsx
@@ -0,0 +1,25 @@
+import React, { PropTypes } from 'react';
+import ToggleCheckBox from 'react-icons/lib/md/check-box';
+import ToggleCheckBoxOutlineBlank from 'react-icons/lib/md/check-box-outline-blank';
+
+import style from '../../styles/style';
+
+const propTypes = {
+  option: PropTypes.bool,
+};
+
+const defaultProps = {
+  option: false,
+};
+
+const CheckboxIcon = ({ option }) => {
+  const CheckBox = option ? ToggleCheckBox : ToggleCheckBoxOutlineBlank;
+  return (
+    <CheckBox style={style.toggleIconButton} />
+  );
+};
+
+CheckboxIcon.propTypes = propTypes;
+CheckboxIcon.defaultProps = defaultProps;
+
+export default CheckboxIcon;
