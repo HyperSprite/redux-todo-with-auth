98779b22207872087cb85226e330cf34e8a23fff
Christopher Dunavan <github@hypersprite.com>
2017-12-07 22:40:53 -0800

    added gear-totals to weekly stats

---
+++ web/src/client/components/gear-totals/readme.md
@@ -0,0 +1,10 @@
+# Gear Totals
+
+This component takes an array of activities ids and returns the total time/distance/elevation broken down by gear used.
+
+Should show the following for each gear type used:
+
+* Gear name
+ * time
+ * distance
+ * elevation
---
+++ web/src/client/components/gear-totals/return-values.js
@@ -0,0 +1,40 @@
+import lib from '../../containers/lib';
+
+// this array will be mapped to display each metric for an activity.
+const returnValues = [
+  {
+    activityType: 'gearName',
+  },
+  {
+    activityType: 'moving_time',
+    activityLabel: 'Moving Time',
+    conversionFunction: lib.statsConversions,
+    conversionMetric: 'time',
+    conversionYAxis: false,
+    conversionData: 'moving_time',
+  },
+  {
+    activityType: 'distance',
+    activityLabel: 'Distance',
+    conversionFunction: lib.statsConversions,
+    conversionMetric: 'dst',
+    conversionYAxis: false,
+    conversionData: 'distance',
+    conversionTypeSA: 'miles',
+    conversionTypeMetric: 'meters',
+    conversionmPref: true,
+  },
+  {
+    activityType: 'total_elevation_gain',
+    activityLabel: 'Elevation',
+    conversionFunction: lib.statsConversions,
+    conversionMetric: 'elev',
+    conversionYAxis: false,
+    conversionData: 'total_elevation_gain',
+    conversionTypeSA: 'feet',
+    conversionTypeMetric: 'meters',
+    conversionmPref: true,
+  },
+];
+
+export default returnValues;
