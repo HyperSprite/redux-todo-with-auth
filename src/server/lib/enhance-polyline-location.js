921b5e978b3dc0a6d2b0ffcfb5f2396faa41dfaa
Christopher Dunavan <github@hypersprite.com>
2018-02-05 13:12:44 -0800

    update enhance-polyline-location to switch extended

--- src/server/lib/enhance-polyline-location.js
+++ src/server/lib/enhance-polyline-location.js
@@ -2,41 +2,58 @@ const mbPolyline = require('polyline');
 const resources = require('./resources');
 
 /**
-* Takes polyline and returns start and end lat/lon/elev
+*
+* extended:
+*   true will call rLonLat to return Elevation path along with geoStart, geoEnd
+*   false will skip rLngLat and only return only geoStart, geoEnd
+*
+* examples of use:
+*   routeplans controller uses extended: true and saves the returned polyline
+*   activites controller uses extended: false as elevatoin is returned from Strava
+*
+* Takes google style polyline and returns:
+* {
+*   geoStart: [-120.2, 38.5], // lng, lat
+*   geoEnd: [-120.95, 40.7], // /lng, lat
+*   elevatoinPath: [
+*     {
+*       "elevation" : 1987.576904296875,
+*       "location" : {
+*       "lng" : -120.1198445639192,
+*       "lat" : 39.26963554253002
+*       },
+*     },
+*   ]
+*   elevationStart: 1987.576904296875 // Number
+*   elevationEnd: 1947.576904296875 // Number
+* }
+*
+* Note: polyline decode returns array of arrays
+*   [[38.5, -120.2], [40.7, -120.95]]
+*   [[ lat,   lng ], [ lat,    lng ]]
 */
-module.exports = (polyline, result) => {
+module.exports = (polyline, extended, result) => {
   if (!polyline) {
     result({});
   }
   const cooArr = mbPolyline.decode(polyline);
-  /**
-  * polyline decode returns array of arrays
-  *   [[38.5, -120.2], [40.7, -120.95]]
-  *   [[ lat,   lng ], [ lat,    lng ]]
-  */
   const geoStart = [cooArr[0][1], cooArr[0][0]];
   const geoEnd = [cooArr[cooArr.length - 1][1], cooArr[cooArr.length - 1][0]];
-  resources.rLonLat({ loc: polyline, samples: 500 }, 'elevationpath', (rslt) => {
-    /**
-    * rLatLng returns array of objects
-    * {
-        "elevation" : 1987.576904296875,
-        "location" : {
-        "lng" : -120.1198445639192,
-        "lat" : 39.26963554253002
-        },
+  if (extended) {
+    resources.rLonLat({ loc: polyline, samples: 500 }, 'elevationpath', (rslt) => {
+      if (rslt.elevationpath && rslt.elevationpath.length) {
+        const rsltElLen = rslt.elevationpath.length;
+        const resultData = {
+          geoStart,
+          geoEnd,
+          elevatoinPath: rslt.elevationpath,
+          elevationStart: rslt.elevationpath[0] && rslt.elevationpath[0].elevation,
+          elevationEnd: rslt.elevationpath[rsltElLen - 1] && rslt.elevationpath[rsltElLen - 1].elevation,
+        };
+        return result(resultData);
       }
-    */
-    if (!rslt.elevationpath.length) {
       return { geoStart, geoEnd };
-    }
-    const resultData = {
-      geoStart,
-      geoEnd,
-      elevatoinPath: rslt.elevationpath,
-      elevationStart: rslt.elevationpath[0] && rslt.elevationpath[0].elevation,
-      elevationEnd: rslt.elevationpath[rslt.elevationpath.length - 1] && rslt.elevationpath[rslt.elevationpath.length - 1].elevation,
-    };
-    return result(resultData);
-  });
+    });
+  }
+  return { geoStart, geoEnd };
 };
