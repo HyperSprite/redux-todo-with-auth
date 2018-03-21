f8aa7c025cbd735efb850520b0c21dbdd4ad0a58
Christopher Dunavan <github@hypersprite.com>
2018-03-21 00:10:09 -0700

    webhooks create-update-delete working

--- src/server/router/webhooks.js
+++ src/server/router/webhooks.js
@@ -5,7 +5,7 @@ const rLib = require('./router-lib');
 const Webhooks = require('./../controllers/webhooks');
 
 // All routes require Auth, see indexRoutes
-router.get('/strava-subscriber', Webhooks.stravaSubscriber);
+// router.get('/strava-subscriber', Webhooks.stravaSubscriber);
 router.get('/strava-receiver', Webhooks.stravaGetReceiver);
 router.post('/strava-receiver', Webhooks.stravaPostReceiver);
 
