819fff22eb165e3da77698d9852790e709f6b047
Christopher Dunavan <github@hypersprite.com>
2018-04-03 23:12:34 -0700

    removed extra props in range-input

--- src/client/components/view-activity-search/range-input.jsx
+++ src/client/components/view-activity-search/range-input.jsx
@@ -11,8 +11,6 @@ const propTypes = {
   valuesDefaults: PropTypes.object.isRequired,
   /** Array */
   valueItems: PropTypes.array.isRequired,
-  /** Object */
-  valuesRange: PropTypes.object.isRequired,
 };
 
 const styles = theme => ({
@@ -46,48 +44,38 @@ class RangeInput extends React.Component {
     let buildRangeArr = [];
     const {
       valueItems,
-      valuesRange,
       valuesDefaults,
     } = this.props;
-    if (valuesRange) {
-      Object.values(valueItems).forEach((element) => {
-        const vals = {
-          conDef0: valuesDefaults[element.value].range[0],
-          conDef1: valuesDefaults[element.value].range[1],
-          conVal0: valuesRange[element.value].range[0],
-          conVal1: valuesRange[element.value].range[1],
-        };
-        if (
-          justFNS.isValid(vals.conDef0) &&
-          justFNS.isValid(vals.conDef1) &&
-          (vals.conDef0 < vals.conDef1)) {
-          if (element.value !== 'count') {
-            buildRangeArr = [...buildRangeArr, {
-              component: 'InputRange',
-              name: element.value,
-              label: element.option,
-              type: 'text',
-              contentDefautls: [
-                justFNS.round(vals.conDef0, 0),
-                justFNS.round(vals.conDef1, 0),
-              ],
-              contentValue: [
-                justFNS.round(vals.conVal0, 0),
-                justFNS.round(vals.conval1, 0),
-              ],
-            }];
-          }
-        } else if (element.value === 'date') {
+    Object.values(valueItems).forEach((element) => {
+      const vals = {
+        conDef0: valuesDefaults[element.value].range[0],
+        conDef1: valuesDefaults[element.value].range[1],
+      };
+      if (
+        justFNS.isValid(vals.conDef0) &&
+        justFNS.isValid(vals.conDef1) &&
+        (vals.conDef0 < vals.conDef1)) {
+        if (element.value !== 'count') {
           buildRangeArr = [...buildRangeArr, {
-            component: 'InputRangeDates',
+            component: 'InputRange',
             name: element.value,
             label: element.option,
-            contentDefautls: [vals.conDef0, vals.conDef1],
-            contentValue: [vals.conVal0, vals.conVal1],
+            type: 'text',
+            contentDefautls: [
+              justFNS.round(vals.conDef0, 0),
+              justFNS.round(vals.conDef1, 0),
+            ],
           }];
         }
-      });
-    }
+      } else if (element.value === 'date') {
+        buildRangeArr = [...buildRangeArr, {
+          component: 'InputRangeDates',
+          name: element.value,
+          label: element.option,
+          contentDefautls: [vals.conDef0, vals.conDef1],
+        }];
+      }
+    });
     this.setState({
       rangeArr: buildRangeArr,
     });
@@ -100,7 +88,6 @@ class RangeInput extends React.Component {
       mPref,
       datePref,
       valueItems,
-      valuesRange,
       valuesDefaults,
       input,
       label,
@@ -115,27 +102,26 @@ class RangeInput extends React.Component {
           {(this.state.rangeArr.map(rA => (
             <div key={rA.name} >
               {rA.name !== 'count' ||
-                  rA.name !== 'maxSpeed' ||
-                  rA.name !== 'maxHeartrate' ||
-                  rA.name !== 'maxWatts' ||
-                  !rA.contentDefautls[0] ? (
-                    <EditSwitch
-                      form={form}
-                      formValues={rA}
-                      defaultValue={rA.contentDefautls}
-                      min={rA.contentDefautls[0]}
-                      max={rA.contentDefautls[1]}
-                      mPref={mPref}
-                      datePref={datePref}
-                      rangeValue={rA.rangeValue}
-                    />
+                rA.name !== 'maxSpeed' ||
+                rA.name !== 'maxHeartrate' ||
+                rA.name !== 'maxWatts' ||
+                !rA.contentDefautls[0] ? (
+                  <EditSwitch
+                    form={form}
+                    formValues={rA}
+                    defaultValue={rA.contentDefautls}
+                    min={rA.contentDefautls[0]}
+                    max={rA.contentDefautls[1]}
+                    mPref={mPref}
+                    datePref={datePref}
+                    rangeValue={rA.rangeValue}
+                  />
               ) : null }
             </div>
           )))}
         </div>
       </div>
     );
-
     return ranges;
   }
 }
@@ -146,7 +132,6 @@ function mapStateToProps(state) {
   const { activities, page, search } = state;
   return {
     valuesDefaults: activities.activCalcAll,
-    valuesRange: activities.activCalcFilter,
     query: search.query,
     valueItems: search.sortStrings,
 
