2c42a31e558f0b32daa1ce12d8d49c9d031cb158
Christopher Dunavan <github@hypersprite.com>
2018-04-04 04:03:47 -0700

    moved rangeInput calc to actions, data now in redux

---
+++ web/src/client/actions/calcRangeInputData.js
@@ -0,0 +1,41 @@
+import justFNS from 'just-fns';
+
+// This builds the array of range inputs for Activity Search
+// Uses API results and then passes them to Redux
+
+const rangeInputData = (activCalcAll, sortStrings) => {
+  const buildRangeArr = Object.values(sortStrings).reduce((accArr, element) => {
+    const vals = {
+      conDef0: activCalcAll[element.value].range[0],
+      conDef1: activCalcAll[element.value].range[1],
+    };
+    if (
+      justFNS.isValid(vals.conDef0) &&
+      justFNS.isValid(vals.conDef1) &&
+      (vals.conDef0 < vals.conDef1)) {
+      if (element.value !== 'count') {
+        return accArr = [...accArr, {
+          component: 'InputRange',
+          name: element.value,
+          label: element.option,
+          type: 'text',
+          contentDefautls: [
+            justFNS.round(vals.conDef0, 0),
+            justFNS.round(vals.conDef1, 0),
+          ],
+        }];
+      }
+    } else if (element.value === 'date') {
+      return accArr = [...accArr, {
+        component: 'InputRangeDates',
+        name: element.value,
+        label: element.option,
+        contentDefautls: [vals.conDef0, vals.conDef1],
+      }];
+    }
+    return accArr;
+  }, Promise.all([]));
+  return buildRangeArr;
+};
+
+export default rangeInputData;
