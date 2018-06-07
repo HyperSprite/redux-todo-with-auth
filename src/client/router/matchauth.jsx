873e6fbf6e927c4f11f230abd6f964693d660559
Christopher Dunavan <github@hypersprite.com>
2018-06-07 12:50:37 -0700

    fix range return to [], not {}. continue work on updaing withRoot

--- src/client/router/matchauth.jsx
+++ src/client/router/matchauth.jsx
@@ -2,21 +2,20 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { Route, Redirect } from 'react-router-dom';
 
-const RouteAuthorized = ({ authenticated, component: Component, ...rest }) => {
-  return (
-    <Route
-      {...rest} render={props => (
-        authenticated ? (
-          <Component {...props} />
-        ) : (
-          <Redirect
-            to={{ pathname: '/home', state: { from: props.location } }}
-          />
-        )
-      )}
-    />
-  );
-};
+const RouteAuthorized = ({ authenticated, component: Component, ...rest }) => (
+  <Route
+    {...rest}
+    render={props => (
+      authenticated ? (
+        <Component {...props} />
+      ) : (
+        <Redirect
+          to={{ pathname: '/home', state: { from: props.location } }}
+        />
+      )
+    )}
+  />
+);
 
 function mapStateToProps(state) {
   return {
