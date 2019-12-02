198bf3cc026086af32ceeac4ba7c7503f0dc4149
Christopher Dunavan <github@hypersprite.com>
2019-12-01 22:18:27 -0800

    update socketIo and mongo remove()

--- src/server/sockets/index.js
+++ src/server/sockets/index.js
@@ -3,11 +3,22 @@ const jwt = require('jwt-simple');
 const hlpr = require('../lib/helpers');
 const ctrlAuth = require('../controllers/authentication');
 
+const socketKeepAlive = 45000; // ping client every 45 seconds
+
 const connections = {};
 
 exports.startSockets = (httpServer) => {
-  const socketio = io.listen(httpServer);
-  socketio.on('connection', (socket) => {
+  const socketIo = io.listen(httpServer);
+  socketIo.on('connection', (socket) => {
+    const id = setInterval(() => {
+      socketIo.send(JSON.stringify(new Date()))
+    }, 45000)
+
+      socketIo.on("close", function() {
+        console.log("websocket connection close")
+        clearInterval(id)
+      })
+
     // connections = socket;
     if (!socket.handshake.secure && process.env.NODE_ENV === 'production') {
       // not secure
@@ -17,7 +28,7 @@ exports.startSockets = (httpServer) => {
     } else if (socket.request.headers.authorization === 'null') {
         // no auth header
       socket.join(socket.id);
-      hlpr.logOutArgs('server socketio', 'try', 'success', 9, null, 'socket', 'socket connect no auth', socket.id);
+      hlpr.logOutArgs('server socketIo', 'try', 'success', 9, null, 'socket', 'socket connect no auth', socket.id);
     } else {
       try {
         const userId = jwt.decode(socket.request.headers.authorization, process.env.AUTH_SECRET);
@@ -29,28 +40,28 @@ exports.startSockets = (httpServer) => {
             type: 'FETCH_USER',
             payload: user,
           });
-          hlpr.logOutArgs('server socketio', 'try', 'success', 9, null, 'socket', 'socket connect', socket.stravaId);
+          hlpr.logOutArgs('server socketIo', 'try', 'success', 9, null, 'socket', 'socket connect', socket.stravaId);
           socket.stravaId = user.stravaId.toString();
           socket.on('action', (action) => {
             if (action.type && action.type.indexOf('SOCKET_CONNECT.') === 0) {
               const actionType = action.type.split('.')[1];
-              socketio.in(user.stravaId).emit('action', {
+              socketIo.in(user.stravaId).emit('action', {
                 type: actionType,
                 payload: user,
               });
-              hlpr.logOutArgs('server socketio', 'try', 'success', 9, null, 'socket', 'sent msg', socket.stravaId);
+              hlpr.logOutArgs('server socketIo', 'try', 'success', 9, null, 'socket', 'sent msg', socket.stravaId);
             }
           });
         });
       } catch (err) {
-        hlpr.logOutArgs('server socketio', 'catch', 'failure', 5, err, 'socket', 'catch err', null);
+        hlpr.logOutArgs('server socketIo', 'catch', 'failure', 5, err, 'socket', 'catch err', null);
         socket.on('end', () => {
           socket.disconnect(0);
         });
       }
     }
   });
-  return socketio;
+  return socketIo;
 };
 
 exports.ifConnected = (stravaId, actionType, actionPayload) => {
